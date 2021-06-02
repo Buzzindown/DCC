0ade8d7e15adda06b1205fe33a48ec1517ce6d7e
Mackenzie Starkebaum <mackenzie.starkebaum@Mackenzie-Starkebaum.local>
2021-06-02 05:45:46 -0400

    full sol

--- June22021.java
+++ June22021.java
@@ -42,6 +42,38 @@ public class June22021 {
     // so we move both posts to 5
     // this seems like it will work
 
+    // this is good but it doesnt cover the case where we have 
+    // [3,5,3,2,3,2]
+    // we would miss the puddle that goes between 3 and 2
+
+    // well that's not gonna work, how should we do this
+    // what if we did something similar from the right side and found 
+    // some difference in the area
+
+    // for the example above, if we started at 2, we would go to 3, then to 2
+    // 2 is less than 3 so we mve the left pole amd add tp tje area whih is 1
+    // then we check 3 < 3 which is false so we move the right pole to the 3,
+    // then we check 3 and 5, move both poles to 5, then over to 3 and we get 1
+    // if we went from the left we would get 0 
+
+    // what about something like this
+    // 
+
+    // what if instead we found a common peak for left/right peaked pools this makes it easier for sure
+    // if we find the peak of the whole map then we're basically filling up 
+    // the space to the left and right of it 
+    // we can keep track of our highest point on the left since we KNOW that we're
+    // peaked to the right, very similar to what I've done already
+    // then we just take a difference between whatever the edge of our current pool is
+    // minus our current elemenet
+    // then we pretty well do the same thing from the other side
+
+    // [4,6,5,12,4,2,3,5,2]
+    // here let's say we find the max for the arr, which is 12
+    // we do the left side first
+    // start with [0] and [1], 4-6 = -2, and 6 > 4 so 6 is our new left edge
+    // 6-5 = 1, 6 < 5
+
     int solution(int[] map){
 
         System.out.println("Solution");
@@ -49,23 +81,28 @@ public class June22021 {
         int rPost = 0;
         int area = 0;
         boolean done = false;
-        // use for checking the right side
         int pool = 0;
 
         while(done == false){
+            
             if(lPost == rPost){
                 // if we put them together than shift them
                 rPost++;
             }
 
          //   System.out.println("Lpost " + lPost + " Rpost " + rPost);
+            // [7,8,4,2,3,5,2]
             // if we're in a hole
             if(map[rPost] < map[lPost]){
+                // height of the bottom of a hole
                 area += map[lPost] - map[rPost];
                 pool += map[lPost] - map[rPost];
                 rPost++;
+
             }else{
                 // if we're not in a hole, we can shift post the posts
+                // left post was the highest we go to, so we know we can go anywehere
+                // between  that and our current r post and still have a poo;
                 lPost = rPost;
                 pool = 0;
             }
@@ -86,7 +123,46 @@ public class June22021 {
 
         return area;
     }
+    
+    int properSolution(int[] arr){
 
+        int arrMax = Integer.MIN_VALUE;
+        int maxIndex = 0;
+        int area = 0;
+
+        // get our central peak
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > arrMax){
+                arrMax = arr[i];
+                maxIndex = i;
+            }
+        }
+
+        int lMax = arr[0];
+        // do our left side 
+        for(int i = 1; i < maxIndex; i++){
+            if(arr[i] <= lMax){
+                area += lMax - arr[i];
+            }
+            if(arr[i] > lMax){
+                lMax = arr[i];
+            }
+        }
+
+        int rMax = arr[arr.length - 1];
+        // right side 
+        for(int i = arr.length - 2; i > maxIndex; i--){
+           // System.out.println("Max " + rMax + " verse " + arr[i]);
+            if(arr[i] <= rMax){
+                area += rMax - arr[i];
+            }
+            if(arr[i] > rMax){
+                rMax = arr[i];
+            }
+        }
+
+        return area;
+    }
     public static void main(String[] args){
 
         Scanner myScanner = new Scanner(System.in);
@@ -106,7 +182,7 @@ public class June22021 {
         myScanner.close();
 
         June22021 t = new June22021();
-        System.out.println(t.solution(arr));
+        System.out.println("Area " + t.properSolution(arr));
 
     }
 }
